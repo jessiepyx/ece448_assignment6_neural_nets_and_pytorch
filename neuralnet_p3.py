aa0f8e698b19f95fe0d844d4b463392a9d8b0334
Jessie Peng <jessie_p@yeah.net>
2020-04-19 05:36:09 -0500

    part 3

--- neuralnet_p3.py
+++ neuralnet_p3.py
@@ -20,8 +20,8 @@ import torch.nn.functional as F
 import torch.optim as optim
 
 
-class NeuralNet(torch.nn.Module):
-    def __init__(self, lrate, loss_fn, in_size, out_size):
+class NeuralNet(nn.Module):
+    def __init__(self, lrate):
         """
         Initialize the layers of your neural network
         @param lrate: The learning rate for the model.
@@ -38,16 +38,30 @@ class NeuralNet(torch.nn.Module):
            self.encoder should be able to take tensor of shape [batch_size, 1, 28, 28] as input.
            self.decoder output tensor should have shape [batch_size, 1, 28, 28].
         """
-        self.encoder = None
-        self.decoder = None
-        self.loss_fn = loss_fn
+        self.lrate = lrate
+
+        self.encoder = nn.Sequential(
+            nn.Conv2d(1, 4, kernel_size=5),
+            nn.ReLU(),
+            nn.Conv2d(4, 16, kernel_size=5),
+            nn.ReLU()
+        )
+
+        self.decoder = nn.Sequential(
+            nn.ConvTranspose2d(16, 4, kernel_size=5),
+            nn.Tanh(),
+            nn.ConvTranspose2d(4, 1, kernel_size=5),
+            nn.Tanh()
+        )
+
+        self.criterion = nn.MSELoss()
+        self.optimizer = optim.Adagrad(self.parameters(), self.lrate, weight_decay=0.001)
 
     def get_parameters(self):
         """ Get the parameters of your network
         @return params: a list of tensors containing all parameters of the network
         """
-        # return self.net.parameters()
-        return []
+        return self.parameters()
 
     def forward(self, x):
         """ A forward pass of your autoencoder
@@ -56,7 +70,7 @@ class NeuralNet(torch.nn.Module):
                       Note that self.decoder output needs to be reshaped from
                       [N, 1, 28, 28] to [N, out_size] beforn return.
         """
-        return torch.zeros(x.shape[0], 28 * 28)
+        return self.decoder(self.encoder(x))
 
     def step(self, x):
         # x [100, 784]
@@ -65,7 +79,19 @@ class NeuralNet(torch.nn.Module):
         @param x: an (N, in_size) torch tensor
         @return L: total empirical risk (mean of losses) at this time step as a float
         """
-        L = 0.0
+        self.optimizer.zero_grad()
+
+        # forward
+        x_hat = self.forward(x)
+
+        # loss
+        loss = self.criterion(x_hat, x)
+        L = loss.item()
+
+        # backward
+        loss.backward()
+        self.optimizer.step()
+
         return L
 
 
@@ -83,5 +109,26 @@ def fit(train_set, dev_set, n_iter, batch_size=100):
     @return net: A NeuralNet object
     # NOTE: This must work for arbitrary M and N
     """
+    # reshape data
+    N = train_set.shape[0]
+    M = dev_set.shape[0]
+    out_size = 28 * 28
+    train_set = train_set.view(N, 1, 28, 28)
+    dev_set = dev_set.view(M, 1, 28, 28)
+
+    # train
+    lrate = 0.01
+    net = NeuralNet(lrate)
+    losses = []
+    for batch in range(n_iter):
+        # get batch data
+        idx = torch.randperm(N)
+        x_batch = train_set[idx[:batch_size]]
+
+        loss = net.step(x_batch)
+        losses.append(loss)
+
+    # develop
+    xhats = net.forward(dev_set).view(M, out_size).detach().numpy()
 
     return losses, xhats, net
